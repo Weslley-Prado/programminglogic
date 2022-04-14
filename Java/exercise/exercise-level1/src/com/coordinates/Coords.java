3f9a0f3a7892f48e99fc1c9790ecd154c29ef987
Weslley Prado <weslley.prado73@gmail.com>
2022-04-14 16:45:47 -0300

    Coords

---
+++ Java/exercise/exercise-level1/src/com/coordinates/Coords.java
@@ -0,0 +1,27 @@
+package com.coordinates;
+
+import java.util.Scanner;
+
+public class Coords {
+
+	public static void main(String[] args) {
+           Scanner sc = new Scanner (System.in);
+           
+           System.out.println("Write coordinates x and y: ");
+           int x = sc.nextInt();
+           int y = sc.nextInt();
+           
+          if (x>=0 && y>=0) {
+               System.out.println("The point is in the first quadrant");
+          } else if (x<0 && y>0) {
+        	  System.out.println("The point is in the second quadrant");
+          } else if (x<0 && y < 0 ) {
+        	  System.out.println("The point is in the third quadrant");
+          } else if (x> 0 && y<0) {
+        	  System.out.println("The point is in the fourth quadrant");
+          } else {
+        	  System.out.println("Write the number valid");
+          }
+	}
+
+}
